12a905d650d640c0d40b0eb56d2be1bbd79a7fc7
bacconsebastian <bacconsebastian@gmail.com>
2023-01-26 10:44:43 -0300

    Fixes visuales paneles

---
+++ client/src/config.js
@@ -0,0 +1,12 @@
+// URLs
+export const BASE_URL = "localhost:3000"
+export const REGISTER_JUGADOR = "08cf22a05f0a036"
+export const REGISTER_ENTRENADOR = "0fb7b2e26ebbcdb"
+
+// UID DEL PRESIDENTE
+export const UID_PRESIDENTE = "SsForRCB55f5xfiyPu0AN51DMQI3";
+
+// USER TYPE HASHED 
+export const PRESIDENTE = "d0ba9ac0f604103f526352abdfd540a53e2f92fb";
+export const JUGADORES = "4473de939f902cf507fd375a7d702c22b454ed8c";
+export const ENTRENADORES = "0c9ad82418949b090f52374a998d52182b6b4399";
