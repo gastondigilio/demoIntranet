c76ff0c32847f71419c7b98823bbe10a272b706a
bacconsebastian <bacconsebastian@gmail.com>
2023-01-26 14:12:15 -0300

    redirect en error permisos

--- client/src/components/ErrorPermisos/ErrorPermisos.jsx
+++ client/src/components/ErrorPermisos/ErrorPermisos.jsx
@@ -6,6 +6,7 @@ export default function ErrorPermisos() {
   return (
     <div className="error-permisos">
       <h2>No posee los permisos para acceder a esta página</h2>
+      <a href="/">Volver al inicio</a>
     </div>
   );
 }
