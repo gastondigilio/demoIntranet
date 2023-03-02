346bff942d587ebaa393b84372f80fc49e65c57d
sbaccon <sbaccon.absti@gmail.com>
2023-03-02 11:07:10 -0300

    cambios cors 4

--- api/src/app.js
+++ api/src/app.js
@@ -3,6 +3,7 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
+const cors = require('cors');
 
 require("./db.js");
 
@@ -10,6 +11,8 @@ const server = express();
 
 server.name = "API";
 
+// Aplicar la configuración de CORS
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
