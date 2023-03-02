b9745c06d6c149f0ef40defeeb2aab70b38d76f4
sbaccon <sbaccon.absti@gmail.com>
2023-03-02 17:11:52 -0300

    Añadiendo .env en docker-compose.yml 5

--- api/src/db.js
+++ api/src/db.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-  "postgres://postgres:1324@postgres/Demo-Intranet",
+  "postgres://postgres:1324@db/Demo-Intranet",
   {
     logging: false,
     native: false,
