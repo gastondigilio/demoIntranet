9550f7a3fa22b06fb8bcdbffaf98f7a18bc6189f
sbaccon <sbaccon.absti@gmail.com>
2023-03-02 10:40:19 -0300

    cambios para ip publica

--- api/src/app.js
+++ api/src/app.js
@@ -14,9 +14,15 @@ server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
-server.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://13.42.144.17");
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
