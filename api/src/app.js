9f532957d1beb66b2b4e4b96f14d019a6c147a50
sbaccon <sbaccon.absti@gmail.com>
2023-03-02 10:46:58 -0300

    cambios cors

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://13.42.144.17");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
